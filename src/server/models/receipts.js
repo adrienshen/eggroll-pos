89572c501090900329fe2170b2e49591e66b7acf
johnbenedictyan <johnyfx00@gmail.com>
2020-03-03 02:16:50 +0000

    Added the receipt model along with its respective functions.

---
+++ src/server/models/receipts.js
@@ -0,0 +1,28 @@
+const _ = require('lodash');
+const db = require('./db');
+
+const Table = () => db('receipts');
+
+class Receipt {
+    constructor(receipt) { this.receipt = receipt }
+    
+    static async create({orderId, paymentMethod, params}) {
+    const res = await Table()
+      .insert({
+        order_id : orderId,
+        payment_method : paymentMethod,
+        subtotal_cents : params.subtotalCents,
+        tax_cents : params.taxCents,
+        total_cents : params.totalCents
+      }).returning('id');
+    return res[0];
+  }
+  
+  static async getWithOrderId(orderId) {
+    return await Table()
+      .select()
+      .where('order_id', orderId)
+      .first();
+  }
+  
+}
