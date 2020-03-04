9a95102c4d04efbbfd10a0b9f1278ff32250af2b
johnbenedictyan <johnyfx00@gmail.com>
2020-03-04 07:28:09 +0000

    Editied the receipt component so that it will also display the information of each of the line items.

--- src/client/js/pages/Receipts.js
+++ src/client/js/pages/Receipts.js
@@ -23,33 +23,71 @@ class Receipt extends React.Component{
   }
   
   render(){
-      if(this.state.receipt === null){
+      if(this.state.receipt === null || this.state.lineItems === null){
         return (
           <h1>Loading</h1>
           )
       } else {
         return (
           <Card style={{width:'100%'}}>
-          <Card.Header><Card.Title className="my-2">Receipt #{this.state.receipt.id}</Card.Title></Card.Header>
-          <Card.Body>
+            <Card.Header><Card.Title className="my-2">Receipt #{this.state.receipt.id}</Card.Title></Card.Header>
+            <Card.Body>
               <Row className="mb-3">
                 <Col>
-                <Card.Text>
-                  <span className="font-weight-bolder">Order ID: </span>{this.state.receipt.order_id}
-                  </Card.Text>
-                </Col>
-                </Row>
-                <Row className="mb-3">
-                <Col>
-                <Card.Text>
-                  <span className="font-weight-bolder">Amount: </span>${this.formatCentsToDollars(this.state.receipt.total_cents)}
+                  <Card.Text>
+                    <span className="font-weight-bolder">Order ID: </span>{this.state.receipt.order_id}
                   </Card.Text>
                 </Col>
               </Row>
-            <ListGroup variant="flush">
-            </ListGroup>
-          </Card.Body>
-          <Card.Footer>{this.state.receipt.created_at}</Card.Footer>
+              <Row className="mb-3">
+                <Col>
+                  <Card.Text>
+                    <span className="font-weight-bolder">Amount: </span>${this.formatCentsToDollars(this.state.receipt.total_cents)}
+                  </Card.Text>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h3>Products bought:</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <ListGroup variant="flush">
+                    {this.state.lineItems.map((item,i) => 
+                      <ListGroup.Item key={i}>
+                        <Col>
+                          <Row>
+                            <Col>
+                              Name:
+                            </Col>
+                            <Col>
+                              {item.name}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col>
+                              Description:
+                            </Col>
+                            <Col>
+                              {item.description}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col>
+                              Price:
+                            </Col>
+                            <Col>
+                              ${this.formatCentsToDollars(item.price_cents)}
+                            </Col>
+                          </Row>
+                        </Col>
+                      </ListGroup.Item>)}
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card.Body>
+            <Card.Footer>{this.state.receipt.created_at}</Card.Footer>
           </Card>
         );
       }
