5893b43b353c92ac5b3c24d09b6efe78f5709186
adrienshen <adrienshen.dev@gmail.com>
2020-03-07 13:45:51 -0500

    Adds heroku database connection string

--- db/knexfile.js
+++ db/knexfile.js
@@ -30,20 +30,16 @@ module.exports = {
   //   },
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
