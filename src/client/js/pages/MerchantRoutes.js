a57a9d47c428e84ebb5d094a7a6929767d0fcffa
adrienshen <adrienshen.dev@gmail.com>
2020-03-08 04:37:06 -0400

    Fetch orders, basic ui migration

--- src/client/js/pages/MerchantRoutes.js
+++ src/client/js/pages/MerchantRoutes.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Grid, Button, ButtonGroup, ButtonToolbar} from 'react-bootstrap';
 import { getTimeUntilPickup } from '../../../shared/orders';
+import {getOrders} from '../api/index';
 
 export default function MerchantRoutes(props) {
   return(
@@ -8,7 +9,7 @@ export default function MerchantRoutes(props) {
       Put your /merchant webview routes here
       see: ReactRouter V4/5
 
-      <MerchantContent />
+      <MerchantOrders />
     </section>
   )
 }
@@ -25,7 +26,27 @@ const orders = [
 
 console.log(getTimeUntilPickup('2020-03-08 07:00:23.758982+08', 15));
 
-function MerchantContent() {
+function MerchantOrders() {
+  const [orders, setOrders] = useState(null);
+
+  useEffect(() => {
+
+    async function fetchOrders() {
+      const MERCHANT_ID = 3;
+      const results = await getOrders(MERCHANT_ID, {});
+      // console.log('GOT ORDERS >> ', results);
+      setOrders(results);
+    }
+
+    fetchOrders();
+
+    // return () => {
+    //   effect
+    // };
+  }, [])
+
+
+  console.log('READY ORDERS >> ', orders);
   return <>
         <div class='nav nav-pills navbar-expand-sm navbar-dark bg-primary'>
             <div class='user-profile'>
@@ -44,12 +65,40 @@ function MerchantContent() {
                 </li>
             </ul>
         </div>
+
+        {orders && Object.keys(orders).map(k => {
+          return <OrderCard order={orders[k]} />;
+        })}
   </>
 }
 
-function OrderCard() {
+function OrderCard({order}) {
   return <section>
-
+    <section class="lt-15"></section>
+    <h3>{order.pickupEta}</h3>
+    <div class="order bg-light clearfix">
+        <div class="left-panel">
+            <button type="button" class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="main-panel">
+          <h4 id="order-id">{order.uuid}</h4>
+            <dl class="list-unstyled">
+                <dt id="item-1">avocado BBT</dt>
+                    <dd>50%</dd>
+                    <dd>pearls</dd>
+            </dl>
+            <h5 id="item-2">brown sugar BBT</h5>
+                <ul>
+                    <li>0%</li>
+                    <li>coffee pearls</li>
+                </ul>
+        </div>
+        <div class="right-panel">
+            <button type="button" class="btn btn-primary">$12.99</button>
+        </div>
+    </div>
   </section>
 }
 
