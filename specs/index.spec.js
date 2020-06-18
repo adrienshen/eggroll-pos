3328d33d3a3513e0e6def64a02a2f4131afadd7c
adrienshen <adrienshen.dev@gmail.com>
2020-06-18 15:25:16 -0700

    Integrate CI

---
+++ specs/index.spec.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+// const app = require('../app.js');
+
+describe('test travisci', () => {
+  it('should return a string', () => {
+    expect('ci with travis').to.equal('ci with travis');
+  });
+});
+
